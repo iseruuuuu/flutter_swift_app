0b7010d636a809b0c511ce3c42f067585d886d66
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-04-28 15:06:33 +0900

    Initial Commit

---
+++ flutter_swift_app/flutter_swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  flutter_swift_appApp.swift
+//  flutter_swift_app
+//
+//  Created by Ryutaro Iseki on 2023/04/28.
+//
+
+import SwiftUI
+
+@main
+struct flutter_swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
